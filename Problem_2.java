c8f814db83a7a2b60cf5bc4a92927726650e366b
Anurag Nitish <2102126.it@gmail.com>
2025-08-21 23:59:31 +0530

    Add files via upload

---
+++ Problem_2.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+public class Problem_2 {
+    public static void main(String args[] ) throws Exception {
+        //BufferedReader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(br.readLine().trim());
+        String[] parts = br.readLine().trim().split(" ");
+        int last = Integer.parseInt(parts[n-1]);
+        if(last%10==0){
+            System.out.println("yes");
+        }
+        else{
+            System.out.println("No");
+        }
+    }
+}
